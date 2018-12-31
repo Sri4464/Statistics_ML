bd673242d61191d6c0d41a8d860c908a12b745c9
Srinivasan Ayyangar <sayyangar@groupon.com>
2018-12-31 12:26:58 -0600

    Random Walk based on Probablity and NumPy(Tossing the Dice)

---
+++ RandomWalk/Random_Walk.py
@@ -0,0 +1,39 @@
+# Importing Packages
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Simulate random walk 500 times
+# If you want the same percentage in every run and in every machine then set the seed
+np.random.seed(123)
+
+all_walks = []
+for i in range(50000):
+    random_walk = [0]
+    for x in range(100):
+        step = random_walk[-1]
+        dice = np.random.randint(1, 7)
+        if dice <= 2:
+            step = max(0, step - 1)
+        elif dice <= 5:
+            step = step + 1
+        else:
+            step = step + np.random.randint(1, 7)
+        if np.random.rand() <= 0.001:
+            step = 0
+        random_walk.append(step)
+    all_walks.append(random_walk)
+
+# Create and plot np_aw_t
+np_aw_t = np.transpose(np.array(all_walks))
+
+# Select last row from np_aw_t: ends
+ends = np_aw_t[-1]
+
+# Plot histogram of ends, display plot
+plt.hist(ends)
+plt.show()
+
+# Getting the percentage of times the step landed is 60 or above
+perc = np.mean(ends >= 60)
+print('{} % of the times we end up at step 60 or greater'.format(str(perc * 100)))
